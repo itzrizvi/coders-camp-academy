588a19f3ae98b6909140ac7e5981ae4cc4faa2ac
itzrizvi <shahriar.me.work@gmail.com>
2021-10-04 13:52:30 +0600

    All Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,23 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    // Text Color Declaration
     textColor: {
       'myText': '#5838fc',
       'white': '#FFFFFF',
       'textPink': '#ff6492',
       'navyBlue': '#2c234d',
     },
+    // Background Color Declaration
     backgroundColor: {
       'bgColor': '#5838fc',
       'bgOffwhite': '#f7f6fa',
     },
+    // Button Background Color Declaration
     buttonColor: {
       'btnBgColor': '#ff6492',
     },
+    // Border Color Declaration
     borderColor: {
       'borderPink': '#ff6492',
       'grayBorder': '#eff2f7'
